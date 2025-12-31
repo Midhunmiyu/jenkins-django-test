0353d25c6edaf0a56c0c84f492570593bf8b822a
Midhunmiyu <midhunrajagopal112@gmail.com>
2025-12-31 13:35:26 +0530

    jenkins test project

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gr_xh-8h3h7umzn+31oi_!qove7@0+#2k89g^wditw#g@qefcm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
